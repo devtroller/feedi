82eeced739583b10649867f9925a7159dceccf7e
Hernan Pereira <pereirahernan@outlook.com>
2022-10-30 13:53:42 -0500

    migrations fail

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,10 +1,13 @@
-import { ConfigType } from '@nestjs/config';
+import { ConfigType, ConfigService } from '@nestjs/config';
 import {
   TypeOrmModuleAsyncOptions,
   TypeOrmModuleOptions,
 } from '@nestjs/typeorm';
 
 import config from '../config';
+
+config();
+const configService = new ConfigService();
 
 export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
   inject: [config.KEY],
@@ -20,8 +23,22 @@ export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
       password,
       database,
       entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      migrations: [__dirname + '/../migrations/*{.ts,.js}'],
       synchronize: true,
       logging: true,
     };
   },
+};
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  port: configService.get('POSTGRES_PORT'),
+  host: configService.get('POSTGRES_HOST'),
+  username: configService.get('POSTGRES_USER'),
+  password: configService.get('POSTGRES_PASSWORD'),
+  database: configService.get('POSTGRES_DB'),
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+  synchronize: true,
+  logging: true,
 };
