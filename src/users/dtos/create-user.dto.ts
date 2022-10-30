facaf6512b7cd41dd7e31fdcf710bc8adfd60109
Hernan Pereira <pereirahernan@outlook.com>
2022-10-30 02:02:18 -0500

    delete params without use

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,10 +1,4 @@
-import {
-  IsString,
-  IsNotEmpty,
-  IsBoolean,
-  IsEmpty,
-  IsOptional,
-} from 'class-validator';
+import { IsString, IsNotEmpty, IsBoolean, IsOptional } from 'class-validator';
 
 //import { ApiProperty } from '@nestjs/swagger';
 
